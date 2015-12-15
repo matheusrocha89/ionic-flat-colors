787834a3aa369d999861a0f1521d2e16fedb386e
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-12-15 08:32:27 -0300

    Creating tasks on gulpfile
    Also rename folder scss to sass.
    TODO: create a task to minify compiled css and rename
    the with .min.css at the end

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
 var sass = require('gulp-sass');
+
+
+gulp.task('sass', function() {
+  gulp.src('./sass/**/*.scss').
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass:watch'])
