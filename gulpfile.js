bd2ef53418a7c26c462395c98b39c69f3d2d0e96
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-12-15 21:53:38 -0300

    Creates task to minify css closes #4
    Also add new npm packages to minify and rename css dist files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 
 gulp.task('sass', function() {
@@ -10,9 +12,17 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./dist/css'));
 });
 
+gulp.task('css-minify', function() {
+  gulp.src('./dist/css/*.css')
+    .pipe(minify())
+    .pipe(rename({
+      'suffix': '.min'
+    }))
+    .pipe(gulp.dest('./dist/css/'));
+});
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch'])
+gulp.task('default', ['sass', 'css-minify'])
