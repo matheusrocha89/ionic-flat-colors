c179cd79ce878f21de5221b1419336b0973ce228
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-12-23 11:55:23 -0300

    Creating controller and HTML page for bars

--- app/bars/bars.js
+++ app/bars/bars.js
@@ -5,4 +5,11 @@ BarsController.$inject = ['$scope', 'colorClasses']
 
 function BarsController($scope, colorClasses){
   var vm = this;
+  vm.classesYellow = colorClasses.yellow;
+  vm.classesBlue = colorClasses.blue;
+  vm.classesGreen = colorClasses.green;
+  vm.classesGray = colorClasses.gray;
+  vm.classesPink = colorClasses.pink;
+  vm.classesRed = colorClasses.red;
+  vm.classesPurple = colorClasses.purple;
 }
