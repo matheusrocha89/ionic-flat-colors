a40f316bcd51ed27624a9e0b23a1338b49f38521
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-12-24 00:39:27 -0300

    Creating initial gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('./sass/ionic-flat-doc.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass'])
